5a8873a4ae45ea99323d9a8717ba75b9f541a00a
zilis012 <32030419+zilis012@users.noreply.github.com>
2017-09-17 02:32:21 -0500

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,18 @@
+currentyear = 2017
+
+puts "What year were you born?"
+birthyear = gets.to_i
+
+puts "What month were you born?(number please)"
+birthmonth = gets.to_i
+
+puts "What day were you born?"
+birthday = gets.to_i
+
+age = currentyear - birthyear
+
+if birthmonth > 9 && birthday > 16
+  print "You are #{age - 1} years old!"
+else
+  print "You are #{age} years old!"
+end
